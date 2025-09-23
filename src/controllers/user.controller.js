d1563fc0e84b9e2341c9c2bd145ad8cab20b9180
raz421 <maharazbhuiyan3@gmail.com>
2025-09-24 00:30:54 +0600

    add reister controller and u fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,46 @@
-// g
+// g m b
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utills/ApiError.js";
+import { ApiRespone } from "../utills/ApiResponse.js";
 import { asyncHandaller } from "../utills/asyncHandaller.js";
+import { uploadToCloudinary } from "../utills/cloudinary.js";
 const registerController = asyncHandaller(async (req, res) => {
-  res.status(200).json({
-    message: "",
+  const { username, email, password, fullname } = req.body;
+  if (
+    [username, fullname, email, password]?.some((field) => field?.trim() === "")
+  ) {
+    throw ApiError(400, "All fields are required");
+  }
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
+  if (existedUser) {
+    throw new ApiError(409, "user or email already  exist");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+  const avatar = await uploadToCloudinary(avatarLocalPath);
+  const coverImage = await uploadToCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "avatar is required");
+  }
+  const user = await User.create({
+    fullname,
+    email,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || null,
+    username: username.toLowerCase(),
+    password,
   });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw ApiError(500, "something went wrong while registering the user "); // g m b
+  }
+  return res
+    .status(201)
+    .json(ApiRespone(200, createdUser, "User reister Successfully"));
 });
 export { registerController };
